ae2ce531c83def741820f944c24314d024946839
Mr-LeeB <quangbaokh9a2@gmail.com>
2022-12-22 11:05:13 +0700

    update

--- app/configs/db.configs.js
+++ app/configs/db.configs.js
@@ -1,7 +1,7 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "123456789",
-    DB: "student_management",
+    HOST: "us-cdbr-east-06.cleardb.net",
+    USER: "b9608f3ebcc8c7",
+    PASSWORD: "c9350fa4",
+    DB: "heroku_9bee6aec8b83136",
     dialect: "mysql",
 }
