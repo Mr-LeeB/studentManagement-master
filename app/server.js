46b930374325cf540e8c7f31b04bf879aa7e84a2
Mr-LeeB <quangbaokh9a2@gmail.com>
2022-12-23 03:44:35 +0700

    update

--- app/server.js
+++ app/server.js
@@ -19,18 +19,18 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
-  });
+});
 
 app.use(cors({
     origin: '*',
     methods: 'POST, PUT, PATCH, GET, DELETE, OPTIONS',
-    headers: '*'
-  }));
+    headers: '*',
+}));
 
 app.get('/products', (req, res) => {
     // Xử lý yêu cầu GET tới /products
-    
-  });
+
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -43,4 +43,4 @@ app.listen(port, () => {
 
 // setup sequelize
 const { sequelize } = require('./model/index');
-sequelize.sync({alter: true}); // chỉ sửa bảng chứ ko xóa bảng
+sequelize.sync({ alter: true }); // chỉ sửa bảng chứ ko xóa bảng
